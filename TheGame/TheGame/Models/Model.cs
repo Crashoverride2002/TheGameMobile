a99d2870ce7fbdbb16c139d3d0e928ab37dfb657
Kent Hansen <HansenKentc@gmail.com>
2020-09-27 16:42:22 +0200

    addition to model.

--- TheGame/TheGame/Models/Model.cs
+++ TheGame/TheGame/Models/Model.cs
@@ -11,8 +11,16 @@ namespace TheGame.Models
     public class Quest
     {
         public int ID { get; set; }
+        
         public int fk_quest_type { get; set; }
         public string QuestName { get; set; }
         public string Description { get; set; }
+    }
+    public class Task
+    {
+        public int ID { get; set; }
+        public int fk_quest_id { get; set; }
+        public string TaskImage { get; set; }
+        public string TaskDescription { get; set; }
     }
 }
