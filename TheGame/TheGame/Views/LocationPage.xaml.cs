3f0fe71063fdb16c7ae89f106028a72170b4d996
HansenKentc@gmail.com <HansenKentc@gmail.com>
2021-02-23 11:12:00 +0100

    Correcting a lot of errors mostly naming due to copy paste at the time.

--- TheGame/TheGame/Views/LocationPage.xaml.cs
+++ TheGame/TheGame/Views/LocationPage.xaml.cs
@@ -16,11 +16,11 @@ namespace TheGame.Views
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class ItemsPage : ContentPage
+    public partial class LocationPage : ContentPage
     {
         ItemsViewModel viewModel;
 
-        public ItemsPage()
+        public LocationPage()
         {
             InitializeComponent();
 
